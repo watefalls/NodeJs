3259b25e99b3c065eddd4a3c6121e7db05be3583
Иванов Тахир <watefalls@mail.ru>
2022-07-12 21:21:36 +0500

    update

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
-const yargs = require("yargs");
-const pkg = require("./package.json");
-const { addNote, printNotes, removeNote } = require("./notes.controller");
+const express = require("express");
+const chalk = require("chalk");
+const {
+  addNote,
+  getNotes,
+  removeNote,
+  updateNote,
+} = require("./notes.controller");
+const path = require("path");
 
-yargs.version(pkg.version);
+const PORT = 3000;
+const app = express();
 
-yargs.command({
-  command: "add",
-  describe: "add new note to list",
-  builder: {
-    title: {
-      type: "string",
-      describe: "Note title",
-      demandOption: true,
-    },
-  },
-  handler({ title }) {
-    addNote(title);
-  },
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", "pages");
+
+app.get("/", async (req, res) => {
+  res.render("index", {
+    title: "Express App",
+    notes: await getNotes(),
+    created: false,
+  });
 });
 
-yargs.command({
-  command: "list",
-  describe: "print all notes",
-  handler() {
-    printNotes();
-  },
+app.post("/", async (req, res) => {
+  await addNote(req.body.title);
+  res.render("index", {
+    title: "Express App",
+    notes: await getNotes(),
+    created: true,
+  });
 });
 
-yargs.command({
-  command: "remove",
-  describe: "Remove note by id",
-  handler({ id }) {
-    removeNote(id);
-  },
+app.delete("/:id", async (req, res) => {
+  await removeNote(req.params.id);
+  res.render("index", {
+    title: "Express App",
+    notes: await getNotes(),
+    created: false,
+  });
 });
 
-yargs.parse();
+app.put("/", async (req, res) => {
+  updateNote(req.body);
+  res.render("index", {
+    title: "Express App",
+    notes: await getNotes(),
+    created: false,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(chalk.green(`server has been started on port ${PORT}`));
+});
